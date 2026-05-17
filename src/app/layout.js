47f087033c6e578301653e6b2bd23e075ad043f0
Mamun123123123 <almalm90459@gmail.com>
2026-05-17 19:07:27 +0600

    login and registration section

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MainNavbar } from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <MainNavbar />
         {children}
+        <Toaster />
         <Footer />
         </body>
     </html>
